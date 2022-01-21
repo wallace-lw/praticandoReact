ddb7492f4c3506e81f756991f50e22282102f939
Wallace Leonhard <wallacelw22@gmail.com>
2022-01-21 17:47:15 -0300

    state com array e objetos

--- src/App.js
+++ src/App.js
@@ -1,49 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Component } from 'react/cjs/react.production.min';
 
 class App extends Component {
     state = {
-        name: 'Wallace Leonardo',
-        counter : 0
+        posts: [
+          {
+            id: 1,
+            title: 'Título 1',
+            body: 'O corpo 1'
+          },
+          {
+            id: 2,
+            title: 'Título 2',
+            body: 'O corpo 3'
+          },
+          {
+            id: 3,
+            title: 'Título 3',
+            body: 'O corpo 3'
+          }
+        ]
       };
-   
-
-    handlePClick = () => {
-      this.setState({name: 'Cinho'})
-    }
-
-    // modo atual de chamar o this no react usando arrow function do JS
-
-    handleAClick = (event) => {
-      event.preventDefault();
-      const { counter } =  this.state;
-      this.setState({counter: counter + 1})
-    }
+  
 
     render(){
-      const { name, counter } = this.state;
+      const { posts } = this.state;
 
       return (
             <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p onClick={this.handlePClick}>
-                  {name} {counter}
-                </p>
-                <a
-                  onClick={this.handleAClick}
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-              </header>
+              {posts.map(post => (
+                <div key={post.id}>
+                  <h1>{post.title}</h1>
+                  <p>{post.body}</p>
+                </div>
+              ))}
             </div>
           );
-    }
- }
+        }
+      }
 
 export default App;
